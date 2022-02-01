81de0cb683bfb2603674b8f0bf3e8fc4a0ed7230
jaero0725 <dhqkgbs@naver.com>
2022-02-01 14:15:07 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : '최재호',
+    'birthday' : '940725',
+    'gender' : '남자',
+    'job' : '직장인'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '홍길동',
+    'birthday' : '940816',
+    'gender' : '남자',
+    'job' : '개발자'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/6',
+    'name' : '이순신',
+    'birthday' : '940432',
+    'gender' : '남자',
+    'job' : '디자이너'
+  }
+]
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        {
+         customers.map( c => {
+           return (
+            <Customer
+              key ={c.id}   //map 은 key값을 필수로 요구함
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday ={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />  
+           );
+         })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
