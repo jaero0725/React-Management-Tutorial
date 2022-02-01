4f18e6fa98de56b5112602215d8a7f348bc0e2cf
jaero0725 <dhqkgbs@naver.com>
2022-02-01 21:32:43 +0900

    Set Proxy

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import {withStyles} from '@material-ui/core/styles';
 const styles = theme =>({
   root:{
     width:'100%',
-    marginTop :theme.spacing.unit * 3,
+    marginTop :theme.spacing(3),
     overflowX :"auto"
   },
   table :{
@@ -20,34 +20,58 @@ const styles = theme =>({
   }
 })
 
-const customers = [
-  {
-    'id' : 1,
-    'image' : 'https://placeimg.com/64/64/1',
-    'name' : '최재호',
-    'birthday' : '940725',
-    'gender' : '남자',
-    'job' : '직장인'
-  },
-  {
-    'id' : 2,
-    'image' : 'https://placeimg.com/64/64/2',
-    'name' : '홍길동',
-    'birthday' : '940816',
-    'gender' : '남자',
-    'job' : '개발자'
-  },
-  {
-    'id' : 3,
-    'image' : 'https://placeimg.com/64/64/6',
-    'name' : '이순신',
-    'birthday' : '940432',
-    'gender' : '남자',
-    'job' : '디자이너'
-  }
-]
+// const customers = [
+//   {
+//     'id' : 1,
+//     'image' : 'https://placeimg.com/64/64/1',
+//     'name' : '최재호',
+//     'birthday' : '940725',
+//     'gender' : '남자',
+//     'job' : '직장인'
+//   },
+//   {
+//     'id' : 2,
+//     'image' : 'https://placeimg.com/64/64/2',
+//     'name' : '홍길동',
+//     'birthday' : '940816',
+//     'gender' : '남자',
+//     'job' : '개발자'
+//   },
+//   {
+//     'id' : 3,
+//     'image' : 'https://placeimg.com/64/64/6',
+//     'name' : '이순신',
+//     'birthday' : '940432',
+//     'gender' : '남자',
+//     'job' : '디자이너'
+//   }
+// ]
+
+/*
+# 프록시 설정하기 
+ react-scripts의 버전이 2 이상인 경우
+ http-proxy-middleware를 설치해 setupProxy.js라는 파일을 통해 proxy 설정을 해줘야 한다.
+*/
 
 class App extends Component{
+
+  //변경될 수 있는 변수 : state
+  state = {
+    customers : ""
+  }
+
+  //componet가 mount 가 되었을때 실행됨. 
+  componentDidMount(){
+    this.callApi()
+      .then( res => this.setState({customers : res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json(); //body변수에 받아서 넣어줌. 
+  }
+
   render(){
     const {classes} = this.props;
     return (
@@ -65,19 +89,20 @@ class App extends Component{
               </TableRow>
             </TableHead>
             <TableBody>
-              {customers.map( c => {
-                return (
-                  <Customer
-                    key ={c.id}   //map 은 key값을 필수로 요구함
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday ={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />  
-                );
-              })}
+              {this.state.customers ? this.state.customers.map( c => {   //state로 변경되었으므로 
+                  return (
+                    <Customer
+                      key ={c.id}   //map 은 key값을 필수로 요구함
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday ={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />  
+                  );
+                }) : ""
+              }
             </TableBody>
           </Table>
         </Paper>
--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;  //5000번 포트로 
+//const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message:'Hello Express'});
+//app.use(cors()); //cors을 허용 
+
+// app.get('/api/hello', (req, res) => {
+//     res.send({message:'Hello Express'});
+// });
+
+// # 고객 목록을 보여주는 get Api
+// json 형식을 이용하여 주고받음. 
+// React 는 비동기 식으로 서버에 갖다옴. , 프록시 설정 
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id' : 1,
+            'image' : 'https://placeimg.com/64/64/1',
+            'name' : '최재호',
+            'birthday' : '940725',
+            'gender' : '남자',
+            'job' : '직장인'
+          },
+          {
+            'id' : 2,
+            'image' : 'https://placeimg.com/64/64/2',
+            'name' : '홍길동',
+            'birthday' : '940816',
+            'gender' : '남자',
+            'job' : '개발자'
+          },
+          {
+            'id' : 3,
+            'image' : 'https://placeimg.com/64/64/6',
+            'name' : '이순신',
+            'birthday' : '940432',
+            'gender' : '남자',
+            'job' : '디자이너'
+          }
+    ]);
 });
 
 app.listen(port , () => console.log(`Listening on port ${port}`));
+
+// Rest API 구축 
